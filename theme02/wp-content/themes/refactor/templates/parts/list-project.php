0aabb11513f9f863f965070e3216a358efca8e43
Tae Young, Kim <kty0529@gmail.com>
2022-06-06 22:04:31 +0900

    chore: 프로젝트 카드에 클릭 요소 추가

--- theme02/wp-content/themes/refactor/templates/parts/list-project.php
+++ theme02/wp-content/themes/refactor/templates/parts/list-project.php
@@ -5,26 +5,28 @@
   $content = apply_filters( 'the_content', get_the_content( false ) );
 ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class( 'project-card' ); ?>>
-  <div class="project-card-thumbnail">
-    <?php if ( $thumbnail ) { ?>
-      <img loading="lazy" src="<?php echo $thumbnail; ?>" alt="프로젝트 대표 이미지">
-    <?php } ?>
+  <?php if ( $thumbnail ) { ?>
+    <div class="project-card-thumbnail">
+      <a href="<?php echo get_permalink(); ?>">
+        <img loading="lazy" src="<?php echo $thumbnail; ?>" alt="프로젝트 대표 이미지">
+      </a>
 
-    <div class="categories">
-      <?php
-        $terms = get_the_terms( $post->ID, 'project-type' );
+      <div class="categories">
+        <?php
+          $terms = get_the_terms( $post->ID, 'project-type' );
 
-        if ( $terms && ! is_wp_error( $terms ) ) {
-          foreach ( $terms as $term ) {
-            $icon = strtolower($term->name);
-            if ( $file = file_get_contents( get_template_directory_uri() . '/assets/icons/svg/' . $icon . '.svg' ) ) {
-              echo '<div class="icon icon-' . $icon . '" data-tooltip="' . strtoupper($term->name) . '">' . $file . '</div>';
+          if ( $terms && ! is_wp_error( $terms ) ) {
+            foreach ( $terms as $term ) {
+              $icon = strtolower($term->name);
+              if ( $file = file_get_contents( get_template_directory_uri() . '/assets/icons/svg/' . $icon . '.svg' ) ) {
+                echo '<a href="' . get_term_link( $term->slug, $term->taxonomy ) . '" class="icon icon-' . $icon . '" data-tooltip="' . strtoupper($term->name) . '">' . $file . '</a>';
+              }
             }
           }
-        }
-      ?>
+        ?>
+      </div>
     </div>
-  </div>
+  <?php } ?>
 
   <div class="project-card-container">
     <a href="<?php echo get_permalink(); ?>"><h3 class="title"><?php echo get_the_title(); ?></h3></a>
