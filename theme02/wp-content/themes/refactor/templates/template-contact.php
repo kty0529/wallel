710845bbd5796d66fb51cc633c9c5769260b0c27
Tae Young, Kim <kty0529@gmail.com>
2022-07-04 19:04:37 +0900

    chore: 컨텍폼 양식 코드 백업

--- theme02/wp-content/themes/refactor/templates/template-contact.php
+++ theme02/wp-content/themes/refactor/templates/template-contact.php
@@ -12,7 +12,21 @@
     <div class="row">
       <div class="col-12">
         <div id="contact">
-          <?php echo do_shortcode('[contact-form-7 id="7803" title="문의하기"]'); ?>
+          <?php
+            echo do_shortcode('[contact-form-7 id="7803" title="문의하기"]');
+
+            // 양식 자꾸 분실해서 백업..
+            // <div class="form">
+            //  <div class="form-row">
+            //    <label for="your-name" class="form-label">이름</label>[text* your-name id:your-name]
+            //  </div>
+            //  <div class="form-row">
+            //    <label for="your-email" class="form-label">이메일주소</label>[email* your-email id:your-email]
+            //  </div>
+            //  <div class="form-row">[textarea* your-message id:your-message]</div>
+            //  <div class="form-row form-row-submit">[submit]</div>
+            // </div>
+          ?>
         </div>
       </div>
     </div>
