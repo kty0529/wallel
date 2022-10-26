72238c6f28c6ecfb100e077086ceac215132c6ef
Tae Young, Kim <kty0529@gmail.com>
2022-10-26 11:54:15 +0900

    feat: 404 페이지 접근 시 redirect 처리

--- theme02/wp-content/themes/refactor/404.php
+++ theme02/wp-content/themes/refactor/404.php
@@ -1,6 +1,20 @@
 <?php
   defined( 'ABSPATH' ) OR die( 'This script cannot be accessed directly.' );
 
+  global $wp;
+  $current_url = add_query_arg( array(), $wp->request );
+
+  if ( $current_url == '179' ) {
+    // square 스킨 예전 URL로 접속 시 redirect
+    wp_redirect( home_url( add_query_arg( array(), '/project/square/' ) ) );
+  } else {
+    // 이상한 주소로 접근 시 메인으로 redirect
+    wp_redirect( home_url() );
+  }
+
+  // 죽어랏
+  die();
+
   // pages style
   wp_enqueue_style( 'style', get_theme_file_uri( '/assets/css/404.min.css' ), array( 'core' ), wp_get_theme()->get( 'Version' ), 'all' );
 
