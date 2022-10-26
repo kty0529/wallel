59d63d328492af20bc8c3b3aec42c8239c8e8876
Tae Young, Kim <kty0529@gmail.com>
2022-10-26 19:03:48 +0900

    chore: 코드 스타일 수정

--- theme02/wp-content/themes/refactor/templates/parts/list.php
+++ theme02/wp-content/themes/refactor/templates/parts/list.php
@@ -4,8 +4,8 @@
   $post_type = get_post_type();
 
   if ( $post_type === 'project' ) {
-    get_template_part('templates/parts/list', 'project');
+    get_template_part( 'templates/parts/list', 'project' );
   } else {
-    get_template_part('templates/parts/list', 'default');
+    get_template_part( 'templates/parts/list', 'default' );
   }
 ?>
