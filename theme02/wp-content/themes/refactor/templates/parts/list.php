a6205be791b16dd5c1e012f6ee3dd36d28bcc081
kty0529 <kty0529@gmail.com>
2022-11-26 18:31:53 +0900

    fix: 리스트 오류 수정

--- theme02/wp-content/themes/refactor/templates/parts/list.php
+++ theme02/wp-content/themes/refactor/templates/parts/list.php
@@ -3,7 +3,7 @@
 
   $detect_post_type = in_array( get_post_type(), array( 'project', 'study' ) );
 
-  if ( $post_type ) {
+  if ( $detect_post_type ) {
     get_template_part( 'templates/parts/list', get_post_type() );
   } else {
     get_template_part( 'templates/parts/list', 'default' );
