dc5d2e56c80ffec0e8e379c6f6f31836ac6e3e94
Tae Young, Kim <kty0529@gmail.com>
2022-12-18 17:32:46 +0900

    refactor: study, contact 그리드 제거

--- theme02/wp-content/themes/refactor/archive-study.php
+++ theme02/wp-content/themes/refactor/archive-study.php
@@ -1,37 +1,34 @@
 <?php
   defined( 'ABSPATH' ) OR die( 'This script cannot be accessed directly.' );
 
+  // pages style
+  wp_enqueue_style( 'style', get_theme_file_uri( '/assets/css/study.min.css' ), array( 'core' ), wp_get_theme()->get( 'Version' ), 'all' );
+
   get_header();
 ?>
-<main id="main">
+<main id="main" <?php post_class( 'page-archive-study' ); ?>>
   <div class="container">
-    <div class="row">
-      <div class="col-9 col-tb-9">
-        <section id="study">
-          <header id="study-header">
-            <h2 class="screen-reader-text">스터디</h2>
-          </header>
+    <section id="study">
+      <header id="study-header">
+        <h2 class="screen-reader-text">스터디</h2>
+      </header>
 
-          <div id="study-container">
-            <?php
-              if ( have_posts() ) {
-                while ( have_posts() ) {
-                  the_post();
+      <div id="study-container">
+        <?php
+          if ( have_posts() ) {
+            while ( have_posts() ) {
+              the_post();
 
-                  get_template_part( 'templates/parts/list' );
-                }
+              get_template_part( 'templates/parts/list' );
+            }
 
-                get_template_part('templates/parts/pagination');
-              }
-            ?>
-          </div>
-        </section>
+            get_template_part('templates/parts/pagination');
+          }
+        ?>
       </div>
+    </section>
 
-      <div class="col-3 col-tb-3">
-        <?php get_sidebar( 'study' ); ?>
-      </div>
-    </div>
+    <?php get_sidebar( 'study' ); ?>
   </div>
 </main>
 <?php
--- theme02/wp-content/themes/refactor/templates/template-contact.php
+++ theme02/wp-content/themes/refactor/templates/template-contact.php
@@ -9,30 +9,29 @@
   // https://pineco.de/snippets/remove-p-tag-from-contact-form-7/
   add_filter('wpcf7_autop_or_not', '__return_false');
 
+  // pages style
+  wp_enqueue_style( 'style', get_theme_file_uri( '/assets/css/contact-form.min.css' ), array( 'core' ), wp_get_theme()->get( 'Version' ), 'all' );
+
   get_header();
 ?>
-<main id="main" <?php post_class(); ?>>
+<main id="main" <?php post_class( 'page-contact-form' ); ?>>
   <div class="container">
-    <div class="row">
-      <div class="col-12">
-        <div id="contact">
-          <?php
-            echo do_shortcode('[contact-form-7 id="7803" title="문의하기"]');
+    <div id="contact">
+      <?php
+        echo do_shortcode('[contact-form-7 id="7803" title="문의하기"]');
 
-            // 양식 자꾸 분실해서 백업..
-            // <div class="form">
-            //  <div class="form-row">
-            //    <label for="your-name" class="form-label">이름</label>[text* your-name id:your-name]
-            //  </div>
-            //  <div class="form-row">
-            //    <label for="your-email" class="form-label">이메일주소</label>[email* your-email id:your-email]
-            //  </div>
-            //  <div class="form-row">[textarea* your-message id:your-message]</div>
-            //  <div class="form-row form-row-submit">[submit]</div>
-            // </div>
-          ?>
-        </div>
-      </div>
+        // 양식 자꾸 분실해서 백업..
+        // <div class="form">
+        //  <div class="form-row">
+        //    <label for="your-name" class="form-label">이름</label>[text* your-name id:your-name]
+        //  </div>
+        //  <div class="form-row">
+        //    <label for="your-email" class="form-label">이메일주소</label>[email* your-email id:your-email]
+        //  </div>
+        //  <div class="form-row">[textarea* your-message id:your-message]</div>
+        //  <div class="form-row form-row-submit">[submit]</div>
+        // </div>
+      ?>
     </div>
   </div>
 </main>
