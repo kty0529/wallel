bab56ecce80859529677933fe0bce04f21937f0e
Tae Young, Kim <kty0529@gmail.com>
2022-12-18 17:51:38 +0900

    refactor: 404 마크업 코드 제거

--- theme02/wp-content/themes/refactor/404.php
+++ theme02/wp-content/themes/refactor/404.php
@@ -14,30 +14,3 @@
 
   // 죽어랏
   die();
-
-  // pages style
-  /*
-  wp_enqueue_style( 'style', get_theme_file_uri( '/assets/css/404.min.css' ), array( 'core' ), wp_get_theme()->get( 'Version' ), 'all' );
-
-  get_header();
-?>
-<main id="main">
-  <div class="container">
-    <div class="row">
-      <div class="col-12">
-        <section id="notfound">
-          <header id="notfound-header">
-            <h2 class="title">페이지를 찾을 수 없습니다.</h2>
-          </header>
-
-          <div id="notfound-container">
-            요청하신 페이지를 찾을 수 없습니다.
-            <br>이 페이지가 계속 될 경우 관리자에게 문의해주세요.
-          </div>
-        </section>
-      </div>
-    </div>
-  </div>
-</main>
-<?php
-  get_footer();
