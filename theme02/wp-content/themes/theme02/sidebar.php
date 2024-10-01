79135d4409792396a6794a6701f3e0563553425f
Tae Young, Kim (casper) <kty0529@gmail.com>
2024-10-01 17:18:27 +0900

    chore: sidebar > 검색 폼 타이틀바 추가

--- theme02/wp-content/themes/theme02/sidebar.php
+++ theme02/wp-content/themes/theme02/sidebar.php
@@ -18,28 +18,34 @@
   <h2 class="screen-reader-text">사이드바</h2>
 
   <div class="widget widget-search">
-    <form method="get" action="<?php echo home_url() ?>">
-      <input type="hidden" name="post_type" value="<?php echo $post_type; ?>">
-      <input type="text" name="s" placeholder="<?php echo $search_placeholder_arr[$post_type]; ?>" required value="<?php echo get_search_query(); ?>">
-      <button type="submit" aria-label="검색">
-        <span class="material-symbols-outlined">search</span>
-      </button>
-    </form>
+    <h3 class="widget-title"><?php echo $search_placeholder_arr[$post_type]; ?></h3>
+
+    <div class="widget-content">
+      <form method="get" action="<?php echo home_url() ?>">
+        <input type="hidden" name="post_type" value="<?php echo $post_type; ?>">
+        <input type="text" name="s" placeholder="검색어를 입력하세요." required value="<?php echo get_search_query(); ?>">
+        <button type="submit" aria-label="검색">
+          <span class="material-symbols-outlined">search</span>
+        </button>
+      </form>
+    </div>
   </div>
 
   <div class="widget widget-category">
     <h3 class="widget-title">카테고리</h3>
 
-    <ul class="cat">
-      <?php
-        wp_list_categories( array(
-          'taxonomy' => $taxonomy_arr[$post_type],
-          'title_li' => '',
-          'show_count' => true,
-          'hide_title_if_empty' => false,
-        ) );
-      ?>
-    </ul>
+    <div class="widget-content">
+      <ul class="cat">
+        <?php
+          wp_list_categories( array(
+            'taxonomy' => $taxonomy_arr[$post_type],
+            'title_li' => '',
+            'show_count' => true,
+            'hide_title_if_empty' => false,
+          ) );
+        ?>
+      </ul>
+    </div>
   </div>
 
   <?php /* ?>
