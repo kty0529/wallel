5db1244087defa3cc0edd76655113cd1db243d82
Tae Young, Kim (casper) <kty0529@gmail.com>
2025-11-08 14:26:12 +0900

    refactor: lint 규칙 개선 및 관련 스크립트 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,16 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
-  },
-  extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:react/jsx-runtime",
-  ],
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-  },
+  env: { browser: true, node: true, es6: true },
+  extends: ["eslint:recommended"],
+  parserOptions: { ecmaVersion: 2021, sourceType: "script" },
+  globals: { __: "writable" },
   rules: {
     quotes: ["error", "double"],
+    semi: ["error", "always"],
+    indent: ["error", 2],
+    "no-var": "error",
+    "prefer-const": "error",
+    eqeqeq: "error",
+    "no-unused-vars": ["warn", { argsIgnorePattern: "^_", varsIgnorePattern: "^_$" }],
+    "no-console": ["warn", { allow: ["warn", "error"] }],
   },
 };
