3e3b45f0651dbb95a1a13bf3723d8be07c95048b
Tae Young, Kim (casper) <kty0529@gmail.com>
2026-03-14 00:34:38 +0900

    chore: Project > List > 카테고리, 태그명 노출

--- theme02/templates/parts/list-project.php
+++ theme02/templates/parts/list-project.php
@@ -22,22 +22,6 @@
         <?php echo $thumbnail; ?>
       </a>
 
-      <div class="categories">
-        <?php
-          $terms = get_the_terms( $post->ID, 'project-type' );
-
-          if ( $terms && ! is_wp_error( $terms ) ) {
-            foreach ( $terms as $term ) {
-              $icon = GET_SVG($term->name);
-
-              if ( ! empty( $icon ) ) {
-                echo '<a href="' . get_term_link( $term->slug, $term->taxonomy ) . '" class="icon icon-' . strtolower( $icon['name'] ) . '" data-tooltip="' . $icon['name'] . '">' . $icon['code'] . '</a>';
-              }
-            }
-          }
-        ?>
-      </div>
-
       <?php if ( $closed['status'] ) { ?>
         <div class="closed">프로젝트 운영 종료</div>
       <?php } ?>
@@ -46,6 +30,51 @@
 
   <div class="content">
     <a href="<?php echo get_permalink(); ?>"><h3 class="title"><?php echo get_the_title(); ?></h3></a>
+
+    <?php
+      $category_terms = get_the_terms( $post->ID, 'project-type' );
+      $tag_terms = get_the_terms( $post->ID, 'project-tag' );
+      $category_links = array();
+      $tag_links = array();
+
+      if ( $category_terms && ! is_wp_error( $category_terms ) ) {
+        foreach ( $category_terms as $term ) {
+          $term_link = get_term_link( $term->term_id, $term->taxonomy );
+          $category_links[] = '<a href="' . esc_url( $term_link ) . '">' . esc_html( $term->name ) . '</a>';
+        }
+      }
+
+      if ( $tag_terms && ! is_wp_error( $tag_terms ) ) {
+        foreach ( $tag_terms as $term ) {
+          $term_link = get_term_link( $term->term_id, $term->taxonomy );
+          $tag_links[] = '<a href="' . esc_url( $term_link ) . '">' . esc_html( $term->name ) . '</a>';
+        }
+      }
+    ?>
+
+    <?php if ( ! empty( $category_links ) || ! empty( $tag_links ) ) { ?>
+      <div class="data">
+        <?php if ( ! empty( $category_links ) ) { ?>
+          <div class="cms">
+            <span class="material-symbols-outlined icon" aria-hidden="true">
+              folder
+            </span>
+
+            <?php echo join( ', ', $category_links ); ?>
+          </div>
+        <?php } ?>
+
+        <?php if ( ! empty( $tag_links ) ) { ?>
+          <div class="languages">
+            <span class="material-symbols-outlined icon" aria-hidden="true">
+              sell
+            </span>
+
+            <?php echo join( ', ', $tag_links ); ?>
+          </div>
+        <?php } ?>
+      </div>
+    <?php } ?>
 
     <div class="description"><?php echo $content; ?></div>
   </div>
